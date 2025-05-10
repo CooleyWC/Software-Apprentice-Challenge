e71adf308ab5353394fdb7d1bf96f2984dc32f08
Will Cooley <william.cooley@gmail.com>
2025-05-09 21:24:55 -0400

    feat: add useEffect that fetches the api data and places it in adData state

--- src/AdView.jsx
+++ src/AdView.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+
+const ADS_URL = "http://localhost:3000/fakeDataSet"
 
 function AdView() {
+
+    const [adData, setAdData] = useState({})
+
+    useEffect(()=>{
+        fetch(ADS_URL)
+        .then(res=>res.json())
+        .then(data=>{
+            setAdData(data)
+        })
+    }, [])
+
+    console.log(adData)
+
+
     return (
         <div>
             <h1>AdView</h1>
