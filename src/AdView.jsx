5c2bb44e5e046a8d1a9091e6407351ed8bec49e2
Will Cooley <william.cooley@gmail.com>
2025-05-09 21:54:47 -0400

    feat: attach google analytics results to each ad

--- src/AdView.jsx
+++ src/AdView.jsx
@@ -2,6 +2,35 @@ import {useEffect, useState} from 'react';
 
 const ADS_URL = "http://localhost:3000/fakeDataSet"
 
+const PLATFORM_KEY_MAP = {
+    facebook_ads: {
+        campaign: "campaign_name",
+        adset: "media_buy_name",
+        creative: "ad_name",
+        spend: "spend",
+        impressions: "impressions",
+        clicks: "clicks"
+
+    },
+    twitter_ads: {
+        campaign: "campaign",
+        adset: "ad_group",
+        creative: "image_name",
+        spend: "spend",
+        impressions: "impressions",
+        clicks: "post_clicks"
+        
+    },
+    snapchat_ads: {
+        campaign: "campaign_name",
+        adset: "ad_squad_name",
+        creative: "creative_name",
+        spend: "cost",
+        impressions: "impressions",
+        clicks: "post_clicks"
+    },
+}
+
 function AdView() {
 
     const [adData, setAdData] = useState({})
@@ -18,9 +47,23 @@ function AdView() {
 
 
     function formatAds(adData, platform, googleAnalytics){
-        console.log('adData FB', adData)
-        console.log('platform', platform)
-        console.log('googleAnalytics', googleAnalytics)
+        const ads = adData
+        const keys = PLATFORM_KEY_MAP[platform]
+        
+        const addGoogleResults = ads.map(ad=>{
+            const matchingGoogleResults = googleAnalytics.filter(analitic=>
+                ad[keys.campaign].toLowerCase().includes(analitic.utm_campaign.toLowerCase().split(' ')[0]) &&
+                ad[keys.adset].toLowerCase().includes(analitic.utm_medium.toLowerCase().split(' ')[0]) &&
+                ad[keys.creative].toLowerCase().includes(analitic.utm_content.toLowerCase().split(' ')[0])
+            ) 
+            return {
+                ...ad,
+                matchingGoogleResults
+            }
+        })
+        
+        console.log(addGoogleResults)
+
     }
 
     return (
