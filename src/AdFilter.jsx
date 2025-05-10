fadf8990f97c6498043cd0ea5bad07d58b41d6e3
Will Cooley <william.cooley@gmail.com>
2025-05-10 00:02:56 -0400

    style: add styles to adfilter - hover - colors

--- src/AdFilter.jsx
+++ src/AdFilter.jsx
@@ -9,24 +9,26 @@ function AdFilter({
 
 }) {
     return (
-        <div className='border solid p-6 mb-4'>
-            <label>Search Campaign</label>
-            <input 
-                className='border'
-                placeholder='Enter Campaign Name'
-                value={campSearch}
-                onChange={handleSearchChange}
-            />
+        <div className='border solid p-6 mb-4 flex flex-row gap-2'>
+            <div className='flex items-center gap-2'>
+                <label>Search Campaign</label>
+                <input 
+                    className='border p-2 rounded-sm'
+                    placeholder=' Enter Campaign Name'
+                    value={campSearch}
+                    onChange={handleSearchChange}
+                />
+            </div>
             <button
-                className='border'
+                className='border p-2 rounded-sm hover:bg-slate-100 transition duration-200 ease-in-out'
                 onClick={handleAsc}
             >Sort by Spend Ascending</button>
             <button
-                className='border'
+                className='border p-2 rounded-sm hover:bg-slate-100 transition duration-200 ease-in-out'
                 onClick={handleDesc}
             >Sort by Spend Descending</button>
             <button 
-                className='border'
+                className='border p-2 rounded-sm hover:bg-red-600 hover:text-white transition duration-200 ease-in-out'
                 onClick={handleClear}
             >Clear</button>
         </div>
